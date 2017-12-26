02392faf788fbca26a38f03cdf9f8a1e43b4b66b
Ferdinand Prantl <prantlf@gmail.com>
2017-12-26 14:42:59 +0100

    Fix access to Uri.fsPath in the test

--- test/searchModule.test.js
+++ test/searchModule.test.js
@@ -8,7 +8,7 @@ suite('searchModule', () => {
 	test('should resolve with path for moduleA.js', () => {
 		return definitionProvider.searchModule('../testFiles/test1.js', './moduleA', '')
 			.then(result => {
-				assert.equal(normalize(result.uri._fsPath), normalize(`/${rootPath}testFiles/moduleA.js`));
+				assert.equal(normalize(result.uri.fsPath), normalize(`/${rootPath}testFiles/moduleA.js`));
 			});
 	});
 
